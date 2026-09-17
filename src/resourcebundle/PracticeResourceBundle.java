9738531ccc13d05ca7156b1c8aa73c80e1e93f2b
quocan9999 <trinhquocan999@gmail.com>
2026-09-17 17:17:39 +0700

    Luyện tập với ResourceBundle

--- src/resourcebundle/PracticeResourceBundle.java
+++ src/resourcebundle/PracticeResourceBundle.java
@@ -1,4 +1,16 @@
 package resourcebundle;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class PracticeResourceBundle {
+    public static void main(String[] args)
+    {
+        Locale locale = new Locale("vi");
+
+        ResourceBundle bundle = ResourceBundle.getBundle("resourcebundle.messages", locale);
+
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("goodbye"));
+    }
 }
